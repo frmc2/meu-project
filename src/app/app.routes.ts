b700b228722ab0655b56fbe2b84169fa8f5f1f1e
frmc2 <frmc2@outlook.com>
2026-09-23 17:02:28 -0300

    componentes conectados a URL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BarraBusca } from './barra-busca/barra-busca';
+import { Cadastro } from './cadastro/cadastro';
+import { Vitrine } from './vitrine/vitrine';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: Vitrine, pathMatch: 'full' }
+];
--- src/app/vitrine/vitrine.ts
+++ src/app/vitrine/vitrine.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-vitrine',
+  standalone: true,
   imports: [],
   templateUrl: './vitrine.html',
   styleUrl: './vitrine.css',
